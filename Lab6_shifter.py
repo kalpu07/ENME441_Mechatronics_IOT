23179c4d73f45af3308047a00679684a31c9d43f
kalpu07 <kalp.upadhayay2022@gmail.com>
2025-10-20 22:32:40 -0400

    new4

--- Lab6_shifter.py
+++ Lab6_shifter.py
@@ -2,29 +2,40 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-dataPin, latchPin, clockPin = 23, 24, 25
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)
 
-pattern = 0b01100110  # pattern to display
+class Shifter:
+    # Runs every time a Shifter is created
+    def __init__(self, serial_pin, clock_pin, latch_pin):
+        # Assigning passed values to class attributes
+        self.serialPin = serial_pin
+        self.clockPin = clock_pin  
+        self.latchPin = latch_pin
 
-def ping(p):  # ping the clock or latch pin
-    GPIO.output(p, 1)
-    time.sleep(0)
-    GPIO.output(p, 0)
+        # GPIO Configuration - start latch & clock low
+        GPIO.setup(self.serialPin, GPIO.OUT)
+        GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+        GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
 
-def shiftByte(b):  # send a byte of data to the output
-    for i in range(8):
-        GPIO.output(dataPin, b & (1 << i))
-        ping(clockPin)  # add bit to register
-    ping(latchPin)  # send register to output
+    # Private method to ping the clock or latch pin
+    def __ping(self, pin):
+        GPIO.output(pin, 1)
+        time.sleep(0)
+        GPIO.output(pin, 0)
 
-try:
-    while 1:
-        for i in range(2**8):
-            shiftByte(i)
-            time.sleep(0.5)
-except:
-    GPIO.cleanup()
+    # Public method to send a byte of data to the output
+    def shiftByte(self, data_byte):
+        for i in range(8):
+            GPIO.output(self.serialPin, data_byte & (1 << i))
+            self.__ping(self.clockPin)  # add bit to register
+        self.__ping(self.latchPin)  # send register to output
 
+# Test code - you can remove this later
+if __name__ == "__main__":
+    try:
+        shifter = Shifter(23, 25, 24)  # data, clock, latch pins
+        while True:
+            for i in range(2**8):
+                shifter.shiftByte(i)
+                time.sleep(0.5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
