1e165ab23574e6497f47c1afe183ccb04f887fe2
kalpu07 <kalp.upadhayay2022@gmail.com>
2025-10-20 22:56:22 -0400

    new7

--- Lab6_shifter.py
+++ Lab6_shifter.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import random
 
 GPIO.setmode(GPIO.BCM)
 
@@ -30,3 +31,51 @@ class Shifter:
             GPIO.output(self.serialPin, data_byte & (1 << i))
             self.__ping(self.clockPin)  # add bit to register
         self.__ping(self.latchPin)  # send register to output
+
+# ------------------------Bug Class-----------------------------------------
+class Bug:
+    
+    def __init__(self, serial_pin, clock_pin, latch_pin, timestep=0.1, x=3, isWrapOn=False):
+        # Set attributes with default values
+        self.timestep = timestep
+        self.x = x
+        self.isWrapOn = isWrapOn
+        
+        # Private shifter object
+        self.__shifter = Shifter(serial_pin, clock_pin, latch_pin)
+        
+        # Track if bug is moving
+        self.is_moving = False
+        
+        # Show starting position
+        self.__shifter.shiftByte(1 << self.x)
+
+    def start(self):
+        """Start the bug moving"""
+        self.is_moving = True
+
+    def stop(self):
+        """Stop the bug and turn off LED"""
+        self.is_moving = False
+        self.__shifter.shiftByte(0)
+
+    def move(self):
+        """Move the bug one step - call this repeatedly"""
+        if not self.is_moving:
+            return
+            
+        # Random step: left or right
+        step = random.choice([-1, 1])
+        new_x = self.x + step
+        
+        # Handle movement based on wrap mode
+        if self.isWrapOn:
+            # Wrap around edges
+            self.x = new_x % 8
+        else:
+            # Bounce off edges
+            if 0 <= new_x <= 7:
+                self.x = new_x
+        
+        # Update LED display
+        self.__shifter.shiftByte(1 << self.x)
--- bug.py
+++ bug.py
@@ -1,41 +1,73 @@
 import RPi.GPIO as GPIO
 import time
 import random
-from Lab6_shifter import Shifter  # Import your Shifter class
+from Lab6_shifter import Bug  # Import Bug class
 
 # Initialize GPIO
 GPIO.setmode(GPIO.BCM)
 
-# a. Instantiate a Shifter object
-shifter = Shifter(23, 25, 24)  # serial, clock, latch pins
+# Switch pins
+s1_pin = 26  # On/Off switch
+s2_pin = 19  # Wrap toggle switch  
+s3_pin = 13  # Speed switch
 
-# Initialize variables
-current_position = 3  # Start in the middle (position 3)
-timestep = 0.05  # 0.05 second time step
+# Setup switch pins as inputs
+GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Create Bug with default values
+bug = Bug(23, 25, 24)
+
+# Track states
+prev_s2_state = GPIO.input(s2_pin)
+last_move_time = 0
+default_speed = bug.timestep
 
 try:
+    print("Bug Controller Ready!")
+    print("S1: On/Off, S2: Wrap Toggle, S3: 3x Speed")
     
     while True:
-        # b. Random walk: move by -1 or +1 with equal probability
-        step = random.choice([-1, 1])
-        new_position = current_position + step
+        current_time = time.time()
         
-        # Prevent moving beyond left or right edges (0-7)
-        if 0 <= new_position <= 7:
-            current_position = new_position
+        # a. Monitor 3 GPIO inputs
+        s1_state = GPIO.input(s1_pin)
+        s2_state = GPIO.input(s2_pin) 
+        s3_state = GPIO.input(s3_pin)
         
-        # Display the current LED position
-        # Convert position to bit pattern (1 << position lights just that LED)
-        led_pattern = 1 << current_position
-        shifter.shiftByte(led_pattern)
+        # b. S1 controls On/Off
+        if s1_state == GPIO.HIGH:
+            if not bug.is_moving:
+                bug.start()
+        else:
+            if bug.is_moving:
+                bug.stop()
         
-        # Wait for the time step
-        time.sleep(timestep)
+        # c. S2 flips wrap state on change
+        if s2_state != prev_s2_state:
+            if s2_state == GPIO.HIGH:  # Only flip on button press
+                bug.isWrapOn = not bug.isWrapOn
+                print(f"Wrap: {'ON' if bug.isWrapOn else 'OFF'}")
+            prev_s2_state = s2_state
+        
+        # d. S3 controls speed (3x when pressed)
+        if s3_state == GPIO.HIGH:
+            bug.timestep = default_speed / 3
+        else:
+            bug.timestep = default_speed
+        
+        # Move bug if it's running and time for next step
+        if bug.is_moving and (current_time - last_move_time >= bug.timestep):
+            bug.move()
+            last_move_time = current_time
+        
+        time.sleep(0.01)  # Small delay to reduce CPU load
 
 except KeyboardInterrupt:
-    print("\nRandom walk stopped")
+    print("\nProgram stopped")
 
 finally:
     # Clean up
-    shifter.shiftByte(0)  # Turn off all LEDs
+    bug.stop()
     GPIO.cleanup()
