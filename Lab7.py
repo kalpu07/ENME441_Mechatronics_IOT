98309cfcf1ccf7aef493d264e431ed6c206839af
kalpu07 <kalp.upadhayay2022@gmail.com>
2025-10-27 20:15:49 -0400

    sample1

---
+++ Lab7.py
@@ -0,0 +1,58 @@
+# webserver.py
+#
+# Web server via sockets.
+#
+# When contacted by a client (web browser), send a web page
+# displaying the states of selected GPIO pins.
+#
+# Must run as sudo to access port 80.  
+#
+# Port 8080 is a non-privileged alternative to port 80 that can
+# be used to avoid the need for sudo, if desired.
+
+import socket
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pins = (19,21,22,23,25,26,32,33)
+for p in pins: GPIO.setup(p, GPIO.IN) 
+
+# Generate HTML for the web page:
+def web_page():
+    rows = [f'<tr><td>{p}</td><td>{GPIO.input(p)}</td></tr>' for p in pins]
+    html = """
+        <html>
+        <head> <title>GPIO Pins</title> </head>
+        <body> <h1>Pin States</h1>
+        <table border="1"> <tr><th>Pin</th><th>Value</th></tr>
+        """ + '\n'.join(rows) + """
+        </table>
+        </body>
+        </html>
+        """
+    print(html)
+    return (bytes(html,'utf-8'))   # convert string to UTF-8 bytes object
+     
+# Serve the web page to a client on connection:
+def serve_web_page():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP-IP socket
+    s.bind(('', 80))
+    s.listen(1)  # up to 3 queued connections
+    while True:
+        time.sleep(0.1)
+        print('Waiting for connection...')
+        conn, (client_ip, client_port) = s.accept()     # blocking call
+        request = conn.recv(1024)                 # read request (required even if none)
+        print(f'Connection from {client_ip}')   
+        conn.send(b'HTTP/1.1 200 OK\r\n')         # status line 
+        conn.send(b'Content-type: text/html\r\n') # header (content type)
+        conn.send(b'Connection: close\r\n\r\n')   # header (tell client to close)
+        # send body in try block in case connection is interrupted:
+        try:
+            conn.sendall(web_page())                    # body
+        finally:
+            conn.close()
+
+serve_web_page()
